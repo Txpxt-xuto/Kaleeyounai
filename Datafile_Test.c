d444bb1dc41c33390976d97b15b53c7c75dfe7cf
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2026-05-03 20:28:27 +0700

    Update Datafile_Test.c

--- Datafile_Test.c
+++ Datafile_Test.c
@@ -1,163 +1,121 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-
-#define MAX_STUDENTS 300
-
-struct Student {
-    char id[20];
-    char fname[50];
-    char lname[50];
-    int mid;
+#include <math.h>
+struct student
+{
+    char Id[10];
+    char fname[20];
+    char lname[20];
+    int Mid;
     int Final;
     char grade[5];
 };
-struct Student students[MAX_STUDENTS];
-int studentCount = 0;
-
-
-
-
-
-
-
-
-void calculateGrades()
-{
-    for (int i = 0; i < studentCount; i++)
-    {
-
-        students[i].Final = (students[i].mid / 3) + 30;
-        int total = students[i].Final + students[i].mid;
-
-        if (total >= 90) strcpy(students[i].grade, "A");
-        else if (total >= 80) strcpy(students[i].grade, "+B");
-        else if (total >= 70) strcpy(students[i].grade, "B");
-        else if (total >= 60) strcpy(students[i].grade, "+C");
-        else if (total >= 50) strcpy(students[i].grade, "C");
-        else if (total >= 40) strcpy(students[i].grade, "+D");
-        else if (total >= 30) strcpy(students[i].grade, "D");
-        else strcpy(students[i].grade, "F");
-    }
-}
-
-
-
-
-
-
-
+struct student students[300];
+int count=0;
 
 void openfile()
 {
-    FILE *fp = fopen("students.dat", "r");
 
-    while (studentCount < MAX_STUDENTS && fscanf(fp, "%s %s %s %d",students[studentCount].id,students[studentCount].fname,students[studentCount].lname,&students[studentCount].mid) != EOF)
+    FILE *fp = fopen("students.dat","r");
+    while(fscanf(fp,"%s %s %s %d",students[count].Id,students[count].fname,students[count].lname,&students[count].Mid) != EOF)
     {
-        studentCount++;
+        count++;
     }
     fclose(fp);
 }
 
-
-
-
-
-
-
-void Searchbyname()
+void calculatefinalandgrade()
 {
-    char target[50];
-    printf("Enter name to search: ");
-    scanf("%s", target);
-
-    for(int i = 0; i < studentCount; i++)
+    for(int i=0;i<=count;i++)
     {
-        if(strcmp(students[i].fname, target) == 0)
-        {
-            printf("ID: %9s | NAME: %15s %-20s | MID: %3d | FINAL: %3d | Grade: %2s\n",students[i].id, students[i].fname, students[i].lname,students[i].mid, students[i].Final, students[i].grade);
-        }
+        students[i].Final=(students[i].Mid/3)+30;
+        int total = students[i].Final + students[i].Mid;
+        if(total>90) strcpy( students[i].grade,"A");
+        else if(total>80) strcpy( students[i].grade,"+B");
+        else if(total>70) strcpy( students[i].grade,"B");
+        else if(total>60) strcpy( students[i].grade,"+C");
+        else if(total>50) strcpy( students[i].grade,"C");
+        else if(total>40) strcpy( students[i].grade,"+D");
+        else if(total>30) strcpy( students[i].grade,"D");
+        else strcpy( students[i].grade,"F");
     }
 }
 
-
-
-
+void Searchbyname()
+{
+    char target[20];
+    printf("\nEnter name: ");
+    scanf("%s",target);
+    for(int i=0;i<=count;i++)
+    {
+        if(strcmp(students[i].fname,target)==0)
+        {
+            printf("%10s %20s %20s %3d %3d %3s\n",students[i].Id,students[i].fname,students[i].lname,students[i].Mid, students[i].Final, students[i].grade);
+        }
+    }
+}
 
 
 void Searchbygrade()
 {
-    char targetGrade[5];
-    printf("Enter grade to search (e.g., A or +B): ");
-    scanf("%s", targetGrade);
-
-    for(int i = 0; i < studentCount; i++)
+    char target[20];
+    printf("\nEnter grade: ");
+    scanf("%s",target);
+    for(int i=0;i<=count;i++)
     {
-        if(strcmp(students[i].grade, targetGrade) == 0)
+        if(strcmp(students[i].grade,target)==0)
         {
-            printf("ID: %9s | NAME: %15s %-20s | MID: %3d | FINAL: %3d | Grade: %2s\n",students[i].id, students[i].fname, students[i].lname,students[i].mid, students[i].Final, students[i].grade);
+            printf("%10s %20s %20s %3d %3d %3s\n",students[i].Id,students[i].fname,students[i].lname,students[i].Mid, students[i].Final, students[i].grade);
         }
     }
 }
 
-
-
-
-
-void Sortbyscore()
+void sort()
 {
-    for (int i = 0; i < studentCount - 1; i++)
+    for(int i=0;i<=count-1;i++)
     {
-        for (int j = 0; j < studentCount - i - 1; j++)
+        for(int j=0;j<=count-i-1;j++)
         {
-            if ((students[j].mid + students[j].Final) < (students[j+1].mid + students[j+1].Final))
+            if(students[j].Mid+students[j].Final<students[j+1].Mid+students[j+1].Final)
             {
-                struct Student temp = students[j];
+                struct student temp = students[j];
                 students[j] = students[j+1];
                 students[j+1] = temp;
             }
         }
     }
-    for (int i = 0; i < studentCount; i++)
+    for(int i=0;i<count;i++)
     {
-      printf("ID: %9s | NAME: %15s %-20s | MID: %3d | FINAL: %3d | Grade: %2s\n",students[i].id, students[i].fname, students[i].lname,students[i].mid, students[i].Final, students[i].grade);
+        printf("%10s %20s %20s %3d %3d %3s\n",students[i].Id,students[i].fname,students[i].lname,students[i].Mid, students[i].Final, students[i].grade);
     }
 }
 
 
 
-
-
-int selectmenu()
+int Menu()
 {
-    int choice;
-    printf("--- Student Management System ---");
-    printf("\n1) Search by name");
-    printf("\n2) Search by grade");
-    printf("\n3) Sort by score");
-    printf("\n4) Exit");
-    printf("\nSelect number: ");
-    if(scanf("%d", &choice) != 1) return 4;
-    return choice;
+    int i;
+    printf("====MENU====\n");
+    printf("1) Search by name\n");
+    printf("2) Search by grade\n");
+    printf("3) Sort\n");
+    printf("4) Exit\n");
+    printf("Enter your ans: ");
+    scanf("%d",&i);
+    if(i==4) return 0;
+    return i;
+
 }
-
-
-
-
-
-
-
 int main()
 {
     openfile();
-    calculateGrades();
+    calculatefinalandgrade();
     int menu;
-    do {
-        menu = selectmenu();
-        if(menu == 1) Searchbyname();
-        else if(menu == 2) Searchbygrade();
-        else if(menu == 3) Sortbyscore();
-    } while(menu != 4);
-
-    return 0;
+    do
+    {
+        menu = Menu();
+        if(menu==1) Searchbyname();
+        if(menu==2) Searchbygrade();
+        if(menu==3) sort();
+    } while (menu!=0);
 }
